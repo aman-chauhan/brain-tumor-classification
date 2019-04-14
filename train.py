61447ca956e7df762bebe1542c9aae34b233e3ba
Aman Chauhan <amanchauhan1994@gmail.com>
2019-04-13 22:04:37 -0400

    Classifier Training Code

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from generator import AutoEncoderGenerator
+from generator import AutoEncoderGenerator, ClassifierGenerator
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 from keras.callbacks import CSVLogger
@@ -17,7 +17,7 @@ def get_filepaths(filepath):
     return pd.read_csv(filepath).values.tolist()
 
 
-def build_autoencoder(key, img_size):
+def build_autoencoder(key):
     preprocess = None
     model = None
     epoch = 0
@@ -52,14 +52,56 @@ def build_autoencoder(key, img_size):
     return (model, get_preprocess(), epoch)
 
 
+def build_classifier(key, dropout_rate):
+    preprocess = None
+    model = None
+    epoch = 0
+    model_d = {'densenet': 'dn121', 'inceptionresnet': 'irv2',
+               'inception': 'iv3', 'resnet': 'r50',
+               'vgg': 'vgg', 'xception': 'x'}
+    if key == 'densenet':
+        from models.densenet import get_classifier, get_preprocess
+    elif key == 'inceptionresnet':
+        from models.inceptionresnet import get_classifier, get_preprocess
+    elif key == 'inception':
+        from models.inception import get_classifier, get_preprocess
+    elif key == 'resnet':
+        from models.resnet import get_classifier, get_preprocess
+    elif key == 'vgg':
+        from models.vgg import get_classifier, get_preprocess
+    else:
+        from models.xception import get_classifier, get_preprocess
+
+    if not os.path.exists(os.path.join('weights',
+                                       'clf_{}_{}.h5'.format(key, dropout_rate))):
+        print('Generating new {} model.'.format(key))
+        model = get_classifier(None, dropout_rate, model_d[key])
+        if os.path.exists(os.path.join('weights', 'ae_{}.h5'.format(key))):
+            model.load_weights(os.path.join('weights', 'ae_{}.h5'.format(key)),
+                               by_name=True)
+        model.compile(optimizer='adadelta', loss='mse',
+                      metrics=['categorical_accuracy'])
+    else:
+        print('Fetching {} model from logs.'.format(key))
+        model_path = os.path.join('weights',
+                                  'clf_{}_{}.h5'.format(key, dropout_rate))
+        log_path = os.path.join('logs',
+                                'clf_{}_{}.csv'.format(key, dropout_rate))
+        model = load_model(model_path)
+        logs = pd.read_csv(log_path)
+        epoch = len(logs)
+        del logs
+    return (model, get_preprocess(), epoch)
+
+
 def autoencoder(key, batch_size):
     K.clear_session()
+    model, preprocess, epoch = build_autoencoder(key)
     train = get_filepaths(os.path.join('meta', 'ae_train.csv'))
     valid = get_filepaths(os.path.join('meta', 'ae_valid.csv'))
     meta_file = open(os.path.join('meta', 'ae_meta.json'), 'r')
     min_max = json.load(meta_file)
     img_size = 256
-    model, preprocess, epoch = build_autoencoder(key, img_size)
     train_generator = AutoEncoderGenerator(preprocess, train, min_max,
                                            batch_size, img_size, True, True)
     valid_generator = AutoEncoderGenerator(preprocess, valid, min_max,
@@ -76,7 +118,34 @@ def autoencoder(key, batch_size):
                         initial_epoch=epoch)
 
 
+def classifier(key, batch_size, dropout_rate):
+    K.clear_session()
+    model, preprocess, epoch = build_classifier(key, dropout_rate)
+    train = get_filepaths(os.path.join('meta', 'clf_train.csv'))
+    valid = get_filepaths(os.path.join('meta', 'clf_valid.csv'))
+    meta_file = open(os.path.join('meta', 'clf_meta.json'), 'r')
+    min_max = json.load(meta_file)
+    img_size = 256
+    train_generator = ClassifierGenerator(preprocess, train, min_max,
+                                          batch_size, img_size, True, True)
+    valid_generator = ClassifierGenerator(preprocess, valid, min_max,
+                                          batch_size, img_size, True, True)
+    model_path = os.path.join('weights', 'clf_{}_{}.h5'.format(key, dropout_rate))
+    log_path = os.path.join('logs', 'clf_{}_{}.csv'.format(key, dropout_rate))
+    checkpoint = ModelCheckpoint(filepath=model_path, save_best_only=True)
+    earlystop = EarlyStopping(monitor='val_loss', patience=5, mode='min')
+    csvlogger = CSVLogger(filename=log_path, append=True)
+    model.fit_generator(generator=train_generator, epochs=100, verbose=1,
+                        callbacks=[csvlogger, checkpoint, earlystop],
+                        validation_data=valid_generator,
+                        use_multiprocessing=True, workers=4,
+                        initial_epoch=epoch)
+
+
 if __name__ == '__main__':
     args = sys.argv
     if args[1] == 'autoencoder':
         autoencoder(args[2], int(args[3]))
+    elif args[1] == 'classifier':
+        for i in range(1, 6, 1):
+            classifier(args[2], int(args[3]), i / 10)
